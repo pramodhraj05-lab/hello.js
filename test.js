abb4483e51f43e3c3221c7ac3d6349289dc435a7
pramodhraj05-lab <pramodhraj05@gmail.com>
2026-01-23 13:21:45 +0000

    modified:   test.js

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-alert("Hello World!");
+function sumOfMultiples(p, q) {
+    let sum = 0;
+
+    for (let x of p) {
+        for (let y of q) {
+            if (y !== 0 && x % y === 0) {
+                sum += x;
+                break; 
+            }
+        }
+    }
+
+    return sum;
+}
