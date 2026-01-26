b86b6cd463c5295277a8eb26c21375956db6e878
pramodhraj05-lab <pramodhraj05@gmail.com>
2026-01-26 09:09:44 +0000

    Newconsole

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-function sumOfMultiples(p, q) {
+function sumOfMultiples(f, m) {
     let sum = 0;
 
-    for (let x of p) {
-        for (let y of q) {
-            if (x !== 0 && x % y === 0) {
-                sum += x;
-                break; 
+    for (let num of m) {
+        for (let factor of f) {
+            if (factor !== 0 && num % factor === 0) {
+                sum += num;
+                break; // prevent double counting
             }
         }
     }
 
     return sum;
 }
+const f = [2, 3];
+const m = [3, 4, 5, 6, 7, 8, 9, 10];
 
-const f = [2,3];
-const m=[3,4,5,6,7,8,9,10];
-console.log(sumOfMultiples(f,m));
+console.log(sumOfMultiples(f, m));
